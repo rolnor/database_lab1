3aa843d4cd9b5172d3a89e68ccc9a2593074005f
rolnor <rolnor-8@student.ltu.se>
2021-04-10 16:57:15 +0200

    pop implemented

--- database_lab1/database_lab1.cpp
+++ database_lab1/database_lab1.cpp
@@ -15,7 +15,8 @@ class course_object_stack
     public:
         void push(string name);
         void pop();
-        course_object_stack();
+        void print();
+        course_object_stack(string name, course_object_stack* next_node);
         ~course_object_stack();
 };
 
@@ -25,11 +26,13 @@ class course_node
     private:
         course_node* next_course;
         string course_name;
-        course_object_stack* active_course;
+        course_object_stack* students;
+        course_object_stack* search_students;
     public:
         ~course_node();
         course_node(course_node* nextnode);
         course_node(string input_name);
+        string getCourseName();
         void print();
 };
 
@@ -39,6 +42,8 @@ int main()
 {
     char choise = ' ';
     course_node* main_node = nullptr;
+    course_node* course_traverser = nullptr;
+    string inputString = "";
 
     while (choise != '0')
     {
@@ -55,13 +60,25 @@ int main()
                 }
                 break;
             case '2':
+                cout << "Enter course name: ";
+                cin >> inputString;
+                course_traverser = main_node;
+                while (course_traverser != nullptr)
+                {
+                    if (course_traverser->getCourseName() == inputString)
+                    {
+                        cout << "Course found. Enter name: ";
+                        cin >> inputString;
+                        //push here
+                        course_traverser = nullptr;
+                    }
+                }
                 break;
             case '3':
                 break;
             case '4':
                 if (main_node != nullptr)
                 {
-
                     main_node->print();
                 }
                 break;
@@ -74,19 +91,25 @@ int main()
 
 course_node::~course_node()
 {
-    delete this->active_course;
+    delete this->students;
+    delete this->search_students;
 }
 
 course_node::course_node(course_node* nextnode)
 {
     next_course = nextnode;  
-    active_course = new course_object_stack();
+    students = new course_object_stack();
+    search_students = nullptr;
 };
 
 course_node::course_node(string input_name)
 {
     next_course = nullptr;
     course_name = input_name;
+}
+string course_node::getCourseName()
+{
+    return this->course_name;
 }
 void course_node::print()
 {
@@ -106,15 +129,28 @@ void menu()
 
 void course_object_stack::push(string name)
 {
+    course_object_stack* students = new course_object_stack(name);
+
 }
 
 void course_object_stack::pop()
 {
 }
 
-course_object_stack::course_object_stack()
+void course_object_stack::print()
 {
-    this->next = nullptr;
+    course_object_stack* traverser = this;
+    while(traverser != nullptr)
+    {
+        cout << traverser->name << endl;
+        traverser = traverser->next;
+    }
+}
+
+course_object_stack::course_object_stack(string name, course_object_stack* next_node )
+{
+    this->name = name;
+    this->next = next_node;
 }
 
 course_object_stack::~course_object_stack()
